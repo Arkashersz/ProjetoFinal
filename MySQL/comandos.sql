2a6ecf71117fc32c350ed6ff47ac14d9df48d393
MarllonTerto <marllonwa@outlook.com>
2023-12-03 11:12:56 -0300

    foreigh key db6

--- MySQL/comandos.sql
+++ MySQL/comandos.sql
@@ -5,3 +5,7 @@ CREATE TABLE users (
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+CREATE Table receita(id_user INT PRIMARY KEY AUTO_INCREMENT, nome_da_receita VARCHAR(255) NOT NULL,
+Modo_de_preparo VARCHAR (500) NOT NULL);
+
+Alter Table users ADD CONSTRAINT fk_idcliente Foreign Key (id) REFERENCES (receita) (id_users)
